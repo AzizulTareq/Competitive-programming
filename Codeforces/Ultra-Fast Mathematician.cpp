5358045b960b2ae4b480207c56ba2f970893a149
azizultareq <azizul.tareq@northsouth.edu>
2019-07-18 21:00:25 +1300

    Add files via upload

---
+++ Codeforces/Ultra-Fast Mathematician.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string a, b;
+    cin>>a>>b;
+
+    for(size_t i=0; i<a.length(); ++i)
+    {
+        if(a[i]==b[i])
+        {
+            cout<<"0";
+        }
+        else
+        {
+            cout<<"1";
+        }
+    }
+}
